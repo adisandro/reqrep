4d4277f8cd19b2ca7febb0b756ca1ab0b4102114
Alessio Di Sandro <alessio.disandro@gmail.com>
2025-06-18 23:59:15 +0200

    Add requirement transformation

--- bin/main.py
+++ bin/main.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 from argparse import ArgumentParser
+from operator import eq
 
-from repair.tester import Requirement, Trace
+from repair.tester import Requirement, Trace, Transformation
 
+
+def and_(a, b):
+    return a and b
+
+def or_(a, b):
+    return a or b
 if __name__ == "__main__":
     parser = ArgumentParser(description='Repairs test requirements')
     parser.add_argument('traces', help='Path to the directory containing test traces')
     args = parser.parse_args()
-    # TODO: modify both, calculate percentage of passes before and after
+    # TODO: calculate percentage of passes before and after
     # TODO: create library of transformations, care about the size of the change
-    req1 = Requirement('Req 1', lambda v: v['BL'] <= v['ic'] <= v['TL'] and v['reset'] == 0,
-                       lambda v: v['yout'] == v['ic'])
-    req2 = Requirement('Req 2', lambda v: True, lambda v: v['TL'] >= v['yout'] >= v['BL'])
+    r1 = Requirement('Req 1', 'BL <= ic <= TL and reset == 0', 'yout == ic')
+    r2 = Requirement('Req 2', 'True', 'TL >= yout >= BL')
+    t1 = Transformation('Add term', lambda pre: pre + ' or True', lambda post: post + ' or True')
     trace = Trace('traces/TUI_0001.csv')
-    trace.parse([req1, req2])
+    trace.parse([r1, r2], [t1])
--- src/repair/tester.py
+++ src/repair/tester.py
@@ -7,13 +7,29 @@ class Requirement:
         self.precondition = precondition
         self.postcondition = postcondition
 
-    def check(self, trace):
-        if self.precondition(trace.variables):
-            if self.postcondition(trace.variables):
+    def check(self, values):
+        pre = self.precondition
+        post = self.postcondition
+        for var, value in values.items():
+            pre = pre.replace(var, value)
+            post = post.replace(var, value)
+        if eval(pre):
+            if eval(post):
                 return 'postcondition met'
             else:
                 return 'postcondition not met'
         return 'precondition not met'
+
+
+class Transformation:
+    def __init__(self, name, transform_pre, transform_post):
+        self.name = name
+        self.transform_pre = transform_pre
+        self.transform_post = transform_post
+
+    def apply(self, requirement):
+        return Requirement(requirement.name + ' + ' + self.name, self.transform_pre(requirement.precondition),
+                           self.transform_post(requirement.postcondition))
 
 
 class Trace:
@@ -21,7 +37,7 @@ class Trace:
         self.path = path
         self.variables = {}
 
-    def parse(self, reqs):
+    def parse(self, reqs, transforms):
         with open(self.path) as csvfile:
             print(f'Trace: {self.path}')
             reader = csv.reader(csvfile)
@@ -31,6 +47,13 @@ class Trace:
                         self.variables[var] = j
                     continue
                 print(f'Time {row[self.variables["Time"]]}:')
+                values = {k: row[i] for k, i in self.variables.items()}
                 for req in reqs:
-                    result = req.check(self)
+                    result = req.check(values)
+                    # TODO: return something different than a string result to execute transformation
+                    # TODO: cache transformed requirements?
                     print(f'\tReq {req.name} {result}')
+                    for trans in transforms:
+                        req_trans = trans.apply(req)
+                        result2 = req_trans.check(values)
+                        print(f'\tReq {req_trans.name} {result2}')
