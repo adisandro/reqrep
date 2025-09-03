829b80ec230dd49738042afb8a597f78089a8751
Alessio Di Sandro <alessio.disandro@gmail.com>
2025-09-04 01:24:43 +0200

    Change repair loop to handle pre/post repairs independently

--- src/repair/approach/optimization/optimization.py
+++ src/repair/approach/optimization/optimization.py
@@ -22,20 +22,11 @@ class OptimizationApproach(Approach):
         creator.create("Individual", gp.PrimitiveTree, fitness=creator.FitnessMin)
 
     def _add_to_toolbox(self):
-
-        def random_individual():
-            if random.random() < 0.5:
-                ind = creator.Individual(self.toolbox.expr_pre())
-                ind.target = "pre"
-            else:
-                ind = creator.Individual(self.toolbox.expr_post())
-                ind.target = "post"
-            return ind
-
         # Add approach-specific registrations
-        # self.toolbox.register("individual", tools.initIterate, creator.Individual, self.toolbox.expr_post)
-        self.toolbox.register("individual", random_individual)    
-        self.toolbox.register("population", tools.initRepeat, list, self.toolbox.individual)
+        self.toolbox.register("individual_pre", tools.initIterate, creator.Individual, self.toolbox.expr_pre)
+        self.toolbox.register("individual_post", tools.initIterate, creator.Individual, self.toolbox.expr_post)
+        self.toolbox.register("population_pre", tools.initRepeat, list, self.toolbox.individual_pre)
+        self.toolbox.register("population_post", tools.initRepeat, list, self.toolbox.individual_post)
 
         self.toolbox.register("select", tools.selNSGA2)
         self.toolbox.register("mate", gp.cxOnePoint)
@@ -49,35 +40,25 @@ class OptimizationApproach(Approach):
         self.toolbox.decorate("mutate_pre", gp.staticLimit(key=len, max_value=10))
         self.toolbox.decorate("mutate_post", gp.staticLimit(key=len, max_value=10))
 
-    def _set_ind_fitness(self, ind):
-            if ind.target == "pre":
-
-                r = Requirement("Candidate", self.toolbox,
-                                self.pset_pre, ind,
-                                self.pset_post, self.init_requirement.post)
+    def _set_ind_fitness(self, ind, target):
+            if target == "pre":
+                ind_pre = ind
+                ind_post = self.init_requirement.post
             else:
-                r = Requirement("Candidate", self.toolbox,
-                                self.pset_pre, self.init_requirement.pre,
-                                self.pset_post, ind)
+                ind_pre = self.init_requirement.pre
+                ind_post = ind
+            r = Requirement("Candidate", self.toolbox, self.pset_pre, ind_pre, self.pset_post, ind_post)
+            ind.fitness.values = (r.correctness[f"{target}_cor"][0], r.desirability["des"])
 
-            # Get correctness
-            r_correctness = r.correctness
-            f_cor = r_correctness["cor"][0]
-
-            # Get desirability
-            r_desirability = r.desirability
-            f_des = r_desirability["des"]
-
-            ind.fitness.values = (f_cor, f_des)
-
-    def _repair(self):
+    def _repair(self, target):
         toolbox = self.toolbox
-        pop = toolbox.population(n=10) # Random initial population # TODO adjust number
+        # Random initial population # TODO adjust number
+        pop = toolbox.population_pre(n=10) if target == "pre" else toolbox.population_post(n=10)
         hof = tools.ParetoFront() # Hall of Fame, for keeping track of the best individuals
 
         # (Initial) Evaluation
         for ind in pop:
-            self._set_ind_fitness(ind)
+            self._set_ind_fitness(ind, target)
 
         pop = toolbox.select(pop, len(pop))
 
@@ -89,7 +70,7 @@ class OptimizationApproach(Approach):
 
             # Crossover
             for child1, child2 in zip(offspring[::2], offspring[1::2]):
-                if random.random() < 0.5 and child1.target == child2.target:
+                if random.random() < 0.5:
                     toolbox.mate(child1, child2)
                     del child1.fitness.values
                     del child2.fitness.values
@@ -97,16 +78,13 @@ class OptimizationApproach(Approach):
             # Mutation
             for mutant in offspring:
                 if random.random() < 0.3:
-                    if mutant.target == "pre":
-                        toolbox.mutate_pre(mutant)
-                    else:
-                        toolbox.mutate_post(mutant)
+                    toolbox.mutate_pre(mutant) if target == "pre" else toolbox.mutate_post(mutant)
                     del mutant.fitness.values
 
             # (Re-)evaluation: only individuals whose fitness has changed
             for ind in offspring:
                 if not ind.fitness.valid:
-                    self._set_ind_fitness(ind)
+                    self._set_ind_fitness(ind, target)
 
             # Selection
             pop = toolbox.select(pop + offspring, len(pop))
@@ -122,24 +100,31 @@ class OptimizationApproach(Approach):
         return hof
 
     def repair(self, threshold):
-        # TODO: For now, I am only considering the single best repaired individual
-        # wrt. correctness. extend this to support multiple repaired individuals (?)
+        # pre | post | pre => post | repair what?
+        # 0   | 0    | 1           | must repair pre, must repair post
+        # 0   | 1    | 1           | must repair pre
+        # 1   | 0    | 0           | must repair post
+        # 1   | 1    | 1           | no repair needed
 
-        # (1) Do we need to repair? # TODO check this
-        to_repair = self.init_requirement.correctness["cor"][1] * 100 < threshold
-        if not to_repair:
+        # TODO: For now, I am only considering the single best repaired individual
+        # TODO: wrt. correctness. extend this to support multiple repaired individuals (?)
+
+        # Do we need to repair?
+        to_repair_pre = self.init_requirement.correctness["pre_cor"][1] * 100 < threshold
+        to_repair_post = self.init_requirement.correctness["post_cor"][1] * 100 < threshold
+        if not to_repair_pre and not to_repair_post:
             return None
 
-        # Run the repair
-        hof_repaired = self._repair()
-        best_repaired = min(hof_repaired, key=lambda x: (x.fitness.values[0], x.fitness.values[1]))  # rank by correctness, then desirability
+        # Run the repair: rank by correctness, then desirability
+        best_repaired_pre = self.init_requirement.pre
+        if to_repair_pre:
+            hof_repaired = self._repair("pre")
+            best_repaired_pre = min(hof_repaired, key=lambda x: (x.fitness.values[0], x.fitness.values[1]))
+        best_repaired_post = self.init_requirement.post
+        if to_repair_post:
+            hof_repaired = self._repair("post")
+            best_repaired_post = min(hof_repaired, key=lambda x: (x.fitness.values[0], x.fitness.values[1]))
+        repaired_req = Requirement("Repaired", self.toolbox, self.pset_pre, best_repaired_pre, self.pset_post,
+                                   best_repaired_post)
 
-        if best_repaired.target == "pre":
-            repaired_req = Requirement("Repaired", self.toolbox,
-                                       self.pset_pre, best_repaired,
-                                       self.pset_post, self.init_requirement.post)
-        else:
-            repaired_req = Requirement("Repaired", self.toolbox,
-                                       self.pset_pre, self.init_requirement.pre,
-                                       self.pset_post, best_repaired)
         return repaired_req
