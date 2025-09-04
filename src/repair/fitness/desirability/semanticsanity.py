db16d77017260a51a6c6f39ecf370354ecf47a73
ArenBabikian <aren.babikian@mail.mcgill.ca>
2025-09-04 03:55:23 -0400

    TEMP: Fix the semantic sanity bug

--- src/repair/fitness/desirability/semanticsanity.py
+++ src/repair/fitness/desirability/semanticsanity.py
@@ -19,10 +19,12 @@ class SamplingBasedSanity(SemanticSanity):
             0.0 → candidate has variable correctness across inputs (not tautology/contradiction)
             1.0 → candidate is tautology or contradiction (correctness constant)
         """
-        base_cor_pre = None
-        all_same_pre = True
-        base_cor_post = None
-        all_same_post = True
+        # base_cor_pre = None
+        # all_same_pre = True
+        # base_cor_post = None
+        # all_same_post = True
+        base_cor_merged = None
+        all_same_merged = True
 
         for _ in range(self.n_samples):
             trace = random.choice(trace_suite.traces)
@@ -31,25 +33,33 @@ class SamplingBasedSanity(SemanticSanity):
             i = random.randint(1, len(trace.items) - 1)
             item = trace.items[i]
 
-            cor_pre = eval_tree(requirement.pre, i, item)
-            if base_cor_pre is None:
-                base_cor_pre = cor_pre
-            elif not is_within_margin(cor_pre, base_cor_pre):
-                all_same_pre = False
-                if not all_same_pre and not all_same_post:
-                    break
+        #     cor_pre = eval_tree(requirement.pre, i, item)
+        #     if base_cor_pre is None:
+        #         base_cor_pre = cor_pre
+        #     elif not is_within_margin(cor_pre, base_cor_pre):
+        #         all_same_pre = False
+        #         if not all_same_pre and not all_same_post:
+        #             break
 
-            cor_post = eval_tree(requirement.post, i, item)
-            if base_cor_post is None:
-                base_cor_post = cor_post
-            elif not is_within_margin(cor_post, base_cor_post):
-                all_same_post = False
-                if not all_same_pre and not all_same_post:
-                    break
+        #     cor_post = eval_tree(requirement.post, i, item)
+        #     if base_cor_post is None:
+        #         base_cor_post = cor_post
+        #     elif not is_within_margin(cor_post, base_cor_post):
+        #         all_same_post = False
+        #         if not all_same_pre and not all_same_post:
+        #             break
+
+            cor_merged = eval_tree(requirement.merged, i, item)
+            if base_cor_merged is None:
+                base_cor_merged = cor_merged
+            elif not is_within_margin(cor_merged, base_cor_merged):
+                all_same_merged = False
+                break
 
         # If all robustness values are the same,
         # it's (likely) trivial, i.e. a constant function (tautology/contradiction)
-        return 1.0 if all_same_pre or all_same_post else 0.0
+        # return 1.0 if all_same_pre or all_same_post else 0.0
+        return 1.0 if all_same_merged else 0.0
 
 
 class SymbolicSanity(SemanticSanity):
